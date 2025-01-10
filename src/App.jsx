34ef7f30a099ed40cdc37ade9d8415f68e0ab9aa
SHR <t41296193@gmail.com>
2025-01-10 16:00:41 +0600

    add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState([])
+  const data = async() =>{
+    await axios.get('https://dummyjson.com/carts/1')
+    .then((res)=>{
+         setCount(res.data.products)
+      
+    })
+  }
+ useEffect(()=>{
+   data()
+ },[])
+  console.log(count);
+  
+ 
   return (
     <>
        <h1>hello my first deploying site </h1>
+       {Array.isArray(count) && count?.map((item)=>{
+           return(
+             <img key={item.id} src={item.thumbnail} alt="" />
+          
+           )
+       })
+       }
     </>
   )
 }
